17488342501cd751ec06700c18b11fae8f52d525
Darren <18633059886@163.com>
2018-05-07 16:09:39 -0400

    fetch data

--- js/component-courses.js
+++ js/component-courses.js
@@ -1,2 +1,38 @@
+
+// let courses = '<ul id="app-course">' +
+//         '<li v-for="course in list">{{ course }}</li>' +
+//         '</ul>';
+// let list;
+
+// /* Fetch the returned json object value. */
+// fetch('get_info.php').then(function (response) {
+//     list = JSON.parse(response);
+// });
+
 // export as global since we're not using a module system
-window.Courses = { template: '<div>Classes</div>' }
+window.Courses = Vue.component('Courses', {
+    name: "Courses",
+    data: function () {
+        return {list:null};
+    },
+    beforeRouteEnter (to, from, next) {
+        let list;
+        console.log("123");
+        /* Fetch the returned json object value. */
+        fetch('../get_info.php').then(function (response) {
+            self.list = response.json().then(function (data) {
+                self.list = data.DATA;
+                next(
+                    function (vm) {
+                        vm.list = self.list;
+                    }
+                )
+                console.log(data.DATA);
+            });
+        });
+    },
+    template: '<div id="app-course">' +
+        '<ul id="app-course">' +
+        '<li v-for="course in list">{{ course }}</li>' +
+        '</ul></div>'
+});
