face506a93b7bd6d5a56a7e35f960671752dc087
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2018-05-08 13:53:06 -0400

    Changed template to multiline

--- js/component-tas.js
+++ js/component-tas.js
@@ -19,11 +19,11 @@ window.TAs = Vue.component("TAs", {
             });
     },
 
-    template:
-        "<div v-bind:style=\"{ marginLeft: 100+ 'px', paddingTop: 25+ 'px'}\">" +
-        '<router-link to="/course">Back to Courses</router-link>' +
-        '<ul v-for="TA in TAs">' +
-        "<router-link :to=\"{ name: 'section', params: {ta: TA}}\">{{TA}}</router-link>" +
-        "</ul>" +
-        "</div>"
+    template:`
+        <div v-bind:style=\"{ marginLeft: 100+ 'px', paddingTop: 25+ 'px'}\">
+        <router-link to="/course">Back to Courses</router-link>
+        <ul v-for="TA in TAs">
+        <router-link :to=\"{ name: 'section', params: {ta: TA}}\">{{TA}}</router-link>
+        </ul>
+        </div>`
 });
