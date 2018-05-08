0b54212eb2639a898fd42adcde163773d0c2cd6e
Daniel Chen <chen.daniel2099@gmail.com>
2018-05-08 14:06:49 -0400

    now using multi-line string definitions with `

--- js/component-sections.js
+++ js/component-sections.js
@@ -25,21 +25,22 @@ window.Sections = Vue.component("Sections", {
             .catch(err => next(vm => (vm.error = err.toString())));
     },
 
-    template:
-        '<div class="sections">' +
-        '<div class="loading" v-if="loading">' +
-        "Loading..." +
-        "</div>" +
-        "<div v-else>" +
-        '<router-link to="/course">{{$route.params.course}}</router-link> -> <router-link :to="{ name: \'TAs\', params: { course: $route.params.course }}">TAs</router-link> -> {{$route.params.ta}}' +
-        '<div v-if="error" class="error">' +
-        "  {{ error }}" +
-        "</div>" +
-        '<div v-if="section_data">' +
-        '<div v-for="item in section_data.DATA" class="section_data">' +
-        "<a>{{ item }}</a>" +
-        "</div>" +
-        "</div>" +
-        "</div>" +
-        "</div>"
+    template:`
+        <div class="sections">
+        <div class="loading" v-if="loading">
+        Loading...
+        </div>
+        <div v-else>
+        <router-link to="/course">{{$route.params.course}}</router-link> -> <router-link :to="{ name: \'TAs\', params: { course: $route.params.course }}">TAs</router-link> -> {{$route.params.ta}}
+        <div v-if="error" class="error">
+        {{ error }}
+        </div>
+        <div v-if="section_data">
+        <div v-for="item in section_data.DATA" class="section_data">
+        <a>{{ item }}</a>
+        </div>
+        </div>
+        </div>
+        </div>
+        `
 });
