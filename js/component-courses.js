80d82cc660d64f8b1c65c0f86ff65d4307d8d9e1
Darren <18633059886@163.com>
2018-05-08 16:49:45 -0400

    remove beforerouteenter add error handling

--- js/component-courses.js
+++ js/component-courses.js
@@ -4,30 +4,36 @@ window.Courses = Vue.component("Courses", {
     data: function() {
         return {
             list: null,
-            err: null
+            err: null,
+            warn: false
         };
     },
+
     /* Before this view is rendered, fetch the data from the php file */
-    beforeRouteEnter(to, from, next) {
+    mounted () {
         /* Fetch the returned json object value. */
         fetch("get_info.php")
-            .then(function(response) {
-                response.json().then(function(data) {
-                    next(function(vm) {
-                        vm.list = data.DATA;
-                    });
-                });
-                /* Handle errors in fetching the data */
-            })
-            .catch(function(error) {
-                next(function(vm) {
-                    vm.err = error.toString();
-                });
-                console.log("Here is error in fetching data!");
-            });
+        .then(function (response) {
+            return response.json()
+        })
+        .then(data => {
+            console.log(Array.isArray(data.DATA));
+            if (Array.isArray(data.DATA)) {
+                console.log(data.DATA);
+                this.list = data.DATA;
+            } else {
+                console.log("data.DATA is not an appropriate array!");
+                this.warn = true;
+            }
+        })
+        .catch(error => {
+            this.err = error.toString();
+            console.log("Here is error in fetching data!");
+        });
     },
     template: `<div id="app-course">
         <p v-if=err>{{ err }}</p>
+        <p v-if=warn>"data.DATA is not an appropriate array!"</p>
         <ul id="app-course">
         <li v-for="element in list">
         <router-link :to="{ name:\'TAs\', params:{course: element}}">
