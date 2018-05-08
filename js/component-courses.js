b4353cb3bb04cb6f94d49e696f9020ae7c8d7501
Darren <18633059886@163.com>
2018-05-08 14:29:01 -0400

    remove the list variable declaration, error handling, add comment, run prettier.js

--- js/component-courses.js
+++ js/component-courses.js
@@ -1,40 +1,36 @@
-
-// let courses = '<ul id="app-course">' +
-//         '<li v-for="course in list">{{ course }}</li>' +
-//         '</ul>';
-// let list;
-
-// /* Fetch the returned json object value. */
-// fetch('get_info.php').then(function (response) {
-//     list = JSON.parse(response);
-// });
-
 // export as global since we're not using a module system
-window.Courses = Vue.component('Courses', {
+window.Courses = Vue.component("Courses", {
     name: "Courses",
-    data: function () {
-        return {list:null};
+    data: function() {
+        return {
+            list: null,
+            err: null
+        };
     },
-    beforeRouteEnter (to, from, next) {
-        let list;
-        // console.log("123");
+    /* Before this view is rendered, fetch the data from the php file */
+    beforeRouteEnter(to, from, next) {
         /* Fetch the returned json object value. */
-        fetch('get_info.php').then(function (response) {
-            self.list = response.json().then(function (data) {
-                self.list = data.DATA;
-                next(
-                    function (vm) {
-                        vm.list = self.list;
-                    }
-                )
-                // console.log(data.DATA);
+        fetch("get_info.php")
+            .then(function(response) {
+                response.json().then(function(data) {
+                    next(function(vm) {
+                        vm.list = data.DATA;
+                    });
+                });
+                /* Handle errors in fetching the data */
+            })
+            .catch(function(error) {
+                next(function(vm) {
+                    vm.err = error.toString();
+                });
+                console.log("Here is error in fetching data!");
             });
-        });
     },
-    template: '<div id="app-course">' +
-        '<ul id="app-course">' +
-        '<li v-for="element in list">' +
-        '<router-link :to="{ name:\'TAs\', params:{course: element}}">' +
-        '{{ element }}</router-link></li>' +
-        '</ul></div>'
+    template: `<div id="app-course">
+        <p v-if=err>{{ err }}</p>
+        <ul id="app-course">
+        <li v-for="element in list">
+        <router-link :to="{ name:\'TAs\', params:{course: element}}">
+        {{ element }}</router-link></li>
+        </ul></div>`
 });
