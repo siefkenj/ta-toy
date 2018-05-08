8e060f48bc8bb6dbf63d249d76808b078e6effab
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2018-05-08 13:34:15 -0400

    Ran Prettier to edit styling

--- js/component-tas.js
+++ js/component-tas.js
@@ -1,27 +1,30 @@
 // export as global since we're not using a module system
-window.TAs = Vue.component('TAs', {
-
-        data: function () {
+window.TAs = Vue.component("TAs", {
+    data: function() {
         return {
-            TAs:null,
-            course:null,
-        }},
+            TAs: null,
+            course: null
+        };
+    },
 
-        beforeRouteEnter(to, from, next){
-            fetch("../get_info.php?course="+to.params.course)
-            .then((response)=>{return response.json()})
-            .then((data)=>{
-                next(
-                    (vm) => vm.TAs = data.DATA
-                )
-            }).catch( error => { console.log(error) })
-        },
+    beforeRouteEnter(to, from, next) {
+        fetch("../get_info.php?course=" + to.params.course)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                next(vm => (vm.TAs = data.DATA));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
 
-    template:'<div v-bind:style="{ marginLeft: 100+ \'px\', paddingTop: 25+ \'px\'}">'+
-        '<router-link to="/course">Back to Courses</router-link>'+
-        '<ul v-for="TA in TAs">'+
-            '<router-link :to="{ name: \'section\', params: {ta: TA}}">{{TA}}</router-link>'+
-        '</ul>'+
-    '</div>'
-
-})
+    template:
+        "<div v-bind:style=\"{ marginLeft: 100+ 'px', paddingTop: 25+ 'px'}\">" +
+        '<router-link to="/course">Back to Courses</router-link>' +
+        '<ul v-for="TA in TAs">' +
+        "<router-link :to=\"{ name: 'section', params: {ta: TA}}\">{{TA}}</router-link>" +
+        "</ul>" +
+        "</div>"
+});
