137e28f164f08714b2b2e905bedc3d307df6e907
Daniel Chen <chen.daniel2099@gmail.com>
2018-05-08 10:37:12 -0400

    fixed a bug where clicking on TA name does not go back to list of TAs; Added name for route that direct to list of TAs

--- js/component-sections.js
+++ js/component-sections.js
@@ -2,37 +2,40 @@
 window.Sections = Vue.component('Sections',{
   data(){
       return {
-        loading: null,
+        loading: true,
         section_data: null,
         error: null
     }
   },
+
   beforeRouteEnter (to, from, next) {
-    console.log(to.params);
     let url = "../get_info.php?course="+to.params.course+"&ta="+to.params.ta
-    console.log(url);
-    fetch(url).then((res) => res.json())
+    fetch(url)
+    .then((res) => res.json())
                           .then((data) =>{console.log(data);
                                 next(
-                                  (vm) => vm.section_data = data
+                                  (vm) => {vm.section_data = data;vm.loading = false}
                                 )})
                           .catch((err) => next((vm) => vm.error = err.toString()));
 
   },
+
   template: '<div class="sections">'+
-              '{{$route.params.course}} -> {{$route.params.ta}} -> {{section_data.TYPE}}' +
               '<div class="loading" v-if="loading">'+
                 'Loading...'+
               '</div>'+
+              '<div v-else>' +
+                '<router-link to="/course">{{$route.params.course}}</router-link> -> <router-link :to="{ name: \'TAs\', params: { course: $route.params.course }}">{{$route.params.ta}}</router-link> -> {{section_data.TYPE}}' +
 
-              '<div v-if="error" class="error">'+
-              '  {{ error }}'+
-              '</div>'+
+                '<div v-if="error" class="error">'+
+                '  {{ error }}'+
+                '</div>'+
 
-              '<div v-if="section_data">'+
-                '<div v-for="item in section_data.DATA" class="section_data">'+
-                  '<a>{{ item }}</a>'+
+                '<div v-if="section_data">'+
+                  '<div v-for="item in section_data.DATA" class="section_data">'+
+                    '<a>{{ item }}</a>'+
+                  '</div>'+
                 '</div>'+
               '</div>'+
-          '</div>'
+            '</div>'
 })
