209b9bbc4b24b24c2108305b33954360a6cf3ae3
Darren <18633059886@163.com>
2018-05-08 17:41:22 -0400

    Adjust the data type checking and some format

--- js/component-courses.js
+++ js/component-courses.js
@@ -10,18 +10,26 @@ window.Courses = Vue.component("Courses", {
     },
 
     /* Before this view is rendered, fetch the data from the php file */
-    mounted () {
+    mounted: function () {
         /* Fetch the returned json object value. */
         fetch("get_info.php")
         .then(function (response) {
             return response.json()
         })
         .then(data => {
-            if (Array.isArray(data.DATA)) {
+            // If the data type is courses as expected, set the list attribute
+            // in this component
+            if (data.TYPE == "courses") {
                 console.log(data.DATA);
                 this.list = data.DATA;
-            } else {
+            }
+            // If the data type is error, display the error message
+            else if (data.TYPE == "error") {
                 console.log("data.DATA is not an appropriate array!");
+                this.err = "The error data is getting back from the php file!";
+            }
+            // If it is of other types, tell it to the user
+            else {
                 this.warn = true;
             }
         })
@@ -30,12 +38,15 @@ window.Courses = Vue.component("Courses", {
             console.log("Here is error in fetching data!");
         });
     },
-    template: `<div id="app-course">
-        <p v-if=err>{{ err }}</p>
-        <p v-if=warn>"data.DATA is not an appropriate array!"</p>
-        <ul id="app-course">
-        <li v-for="element in list">
-        <router-link :to="{ name:\'TAs\', params:{course: element}}">
-        {{ element }}</router-link></li>
-        </ul></div>`
+    template:
+        `<div id="app-course">
+            <p v-if=err>{{ err }}</p>
+            <p v-if=warn>"You are not getting back the course list!"</p>
+            <ul id="app-course">
+                <li v-for="element in list">
+                    <router-link :to="{ name:\'TAs\', params:{course: element}}">
+                    {{ element }}</router-link>
+                </li>
+            </ul>
+        </div>`
 });
