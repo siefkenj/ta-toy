2edb77111742c4b5954cbfa6367b0ece33230618
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2018-05-10 10:00:38 -0400

    Implemented 404 error page.

---
+++ js/component-pageNotFound.js
@@ -0,0 +1,8 @@
+// export as global since we're not using a module system
+window.PageNotFound = Vue.component("PageNotFound", {
+    template: `
+        <div class="PageNotFound">
+            <h1>404 Error: Page Not Found</h1>
+        </div>
+        `
+});
