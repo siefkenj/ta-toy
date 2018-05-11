06be8c88cfe92977d8729fae9005ba490838e058
Daniel Chen <chen.daniel2099@gmail.com>
2018-05-11 09:40:59 -0400

    Modified get_info.php with calls to database; added sample sql file and database config file; updated README file with Database setup info;

--- get_info.php
+++ get_info.php
@@ -1,70 +1,48 @@
 <?php
-function arr_get($array, $key, $default = null){
+// Load database configurations
+include ('db/config.php');
+
+function arr_get($array, $key, $default = null)
+{
     return isset($array[$key]) ? $array[$key] : $default;
 }
-
-header('Content-type: application/json');
-
 $course = arr_get($_GET, 'course', '');
 $ta = arr_get($_GET, 'ta', '');
+try {
+    $stmt;
+    if ($course == '') {
+        $stmt = $conn->prepare("SELECT DISTINCT Course FROM courses;");
+        $result['TYPE'] = "courses";
+    }
+    else if ($ta == '') {
+        $stmt = $conn->prepare("SELECT DISTINCT TA FROM courses WHERE Course ='" . $course . "';");
+        $result['TYPE'] = "ta";
+    }
+    else if ($ta != '') {
+        $stmt = $conn->prepare("SELECT DISTINCT Sections FROM courses WHERE Course ='" . $course . "'AND TA = '" . $ta . "';");
+        $result['TYPE'] = "section";
+    }
+    else {
+        $error = 'Invalid params';
+        throw new Exception($error);
+    }
+    $stmt->execute();
+    // set the resulting array to associative
+    $query = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
 
-// list of courses
-if ($course == '') {
-    print('{ "TYPE": "courses", "DATA": ["MAT135H1F", "CSC236H1S", "MAT223H1S"] }');
-    exit(0);
+    if (count($query) >= 1) {
+        $result['STATUS'] = 'OK';
+        $result['DATA'] = $query;
+    }
+    else {
+        $result = array(
+            'STATUS' => 'EMPTY'
+        );
+    }
+    echo json_encode($result);
 }
-
-// list of TAs by course
-if ($course == 'MAT135H1F' && $ta == '') {
-    print('{ "TYPE": "tas", "DATA": ["Jenny Grover", "Martha", "Tim"] }');
-    exit(0);
+catch(PDOException $e) {
+    $result = array('status' => 'ERROR', 'error' => "Connection failed: " . $e->getMessage());
+    print(json_encode($result, JSON_PRETTY_PRINT));
 }
-
-if ($course == 'CSC236H1S' && $ta == '') {
-    print('{ "TYPE": "tas", "DATA": ["Sam Tobble", "Jacob", "Roger"] }');
-    exit(0);
-}
-
-if ($course == 'MAT223H1S' && $ta == '') {
-    print('{ "TYPE": "tas", "DATA": ["Wanda Zhu"] }');
-    exit(0);
-}
-
-// list of sections by (course, TA)
-if ($course == 'MAT135H1F' && $ta == 'Jenny Grover') {
-    print('{ "TYPE": "sections", "DATA": ["TUT0401", "TUT5401"] }');
-    exit(0);
-}
-
-if ($course == 'MAT135H1F' && $ta == 'Martha') {
-    print('{ "TYPE": "sections", "DATA": ["TUT0301", "TUT0201"] }');
-    exit(0);
-}
-
-if ($course == 'MAT135H1F' && $ta == 'Tim') {
-    print('{ "TYPE": "sections", "DATA": ["TUT0101"] }');
-    exit(0);
-}
-
-if ($course == 'CSC236H1S' && $ta == 'Sam Tobble') {
-    print('{ "TYPE": "sections", "DATA": ["TUT0301", "TUT0201"] }');
-    exit(0);
-}
-
-if ($course == 'CSC236H1S' && $ta == 'Jacob') {
-    print('{ "TYPE": "sections", "DATA": ["TUT0101", "TUT5701", "TUT0202", "TUT0123"] }');
-    exit(0);
-}
-
-if ($course == 'CSC236H1S' && $ta == 'Roger') {
-    print('{ "TYPE": "sections", "DATA": ["TUT0203", "TUT0124"] }');
-    exit(0);
-}
-
-if ($course == 'MAT223H1S' && $ta == 'Wanda Zhu') {
-    print('{ "TYPE": "sections", "DATA": ["TUT0203", "LEC0101"] }');
-    exit(0);
-}
-
-    print('{ "TYPE": "error" }');
-?> 
+?>
