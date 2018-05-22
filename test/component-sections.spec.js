dcf5ee132069e55668fea8c53dc047f08f4a1ae4
Darren <18633059886@163.com>
2018-05-21 20:54:12 -0400

    testing framework

--- test/component-sections.spec.js
+++ test/component-sections.spec.js
@@ -1,2 +1,8 @@
 import { shallow } from '@vue/test-utils';
 import tas from '../src/components/component-tas.vue';
+
+describe('component-sections.vue', function () {
+    it('lifecycle-testing', function () {
+
+    });
+});
--- test/component-tas.spec.js
+++ test/component-tas.spec.js
@@ -1,2 +1,8 @@
 import { shallow } from '@vue/test-utils';
 import sections from '../src/components/component-sections.vue';
+
+describe('component-tas.vue', function () {
+    it('lifecycle-testing', function () {
+
+    });
+});
