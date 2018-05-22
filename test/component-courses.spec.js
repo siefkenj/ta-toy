f915ab602e5d6190f6ccc71ccaeef1b75c80ff72
Darren <18633059886@163.com>
2018-05-22 11:43:22 -0400

    1. change shallow to shallowMount 2. import chai 3. Add comments

--- test/component-courses.spec.js
+++ test/component-courses.spec.js
@@ -1,11 +1,14 @@
-import { shallow } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import courses from '../src/components/component-courses.vue';
 import "isomorphic-fetch";
+import chai from 'chai';
+
+let expect = chai.expect;
 
 describe('component-courses.vue', function () {
     it('lifecycle-testing', function () {
         // Create a new 'courses' instance
-        const wrapper = shallow(courses);
+        const wrapper = shallowMount(courses);
         // Use nextTick to ensure that any promises are resolved before the
         // assertion is made
         wrapper.vm.$nextTick(() => {
--- test/component-sections.spec.js
+++ test/component-sections.spec.js
@@ -1,12 +1,13 @@
-import { shallow } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import { createLocalVue } from '@vue/test-utils';
 import VueRouter from "vue-router";
 import sections from '../src/components/component-sections.vue';
 import courses from "../src/components/component-courses.vue";
 import TAs from "../src/components/component-tas.vue";
 import "isomorphic-fetch";
-let expect = require("expect.js");
+import chai from 'chai';
 
+// Create a local vue-router instance in order to use route var
 const localVue = createLocalVue();
 localVue.use(VueRouter);
 const $route = [
@@ -18,11 +19,12 @@ const $route = [
 const router = new VueRouter({
     routes: $route
 });
+let expect = chai.expect;
 
 describe('component-sections.vue', function () {
     it('lifecycle-testing', function () {
         // Create a new sections instance
-        const wrapper = shallow(sections, {
+        const wrapper = shallowMount(sections, {
             localVue,
             router
         });
