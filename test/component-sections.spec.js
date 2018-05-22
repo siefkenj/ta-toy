55d1d002f01ea3a441cfb84de4365020f189be3c
Darren <18633059886@163.com>
2018-05-22 11:10:55 -0400

    Lifecycle testing for all the components successful!

--- test/component-sections.spec.js
+++ test/component-sections.spec.js
@@ -1,15 +1,35 @@
 import { shallow } from '@vue/test-utils';
+import { createLocalVue } from '@vue/test-utils';
+import VueRouter from "vue-router";
 import sections from '../src/components/component-sections.vue';
+import courses from "../src/components/component-courses.vue";
+import TAs from "../src/components/component-tas.vue";
 import "isomorphic-fetch";
+let expect = require("expect.js");
 
-// describe('component-sections.vue', function () {
-//     it('lifecycle-testing', function () {
-//         // Create a new sections instance
-//         const wrapper = shallow(sections);
-//         // Testing when the fetch is resolved
-//         wrapper.vm.$nextTick(() => {
-//             expect(wrapper.vm.loading).to.be.equal(false);
-//             done();
-//         });
-//     });
-// });
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+const $route = [
+    { path: "/course/:course/ta/:ta", name: "section", component: sections },
+    { path: "/course/:course", name: "TAs", component: TAs },
+    { path: "/", component: courses },
+    { path: "/*", component: courses }
+];
+const router = new VueRouter({
+    routes: $route
+});
+
+describe('component-sections.vue', function () {
+    it('lifecycle-testing', function () {
+        // Create a new sections instance
+        const wrapper = shallow(sections, {
+            localVue,
+            router
+        });
+        // Testing when the fetch is resolved
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.vm.loading).to.be.equal(false);
+            done();
+        });
+    });
+});
--- test/component-tas.spec.js
+++ test/component-tas.spec.js
@@ -1,15 +1,36 @@
 import { shallow } from '@vue/test-utils';
-import tas from '../src/components/component-tas.vue';
+import { createLocalVue } from '@vue/test-utils';
+import VueRouter from "vue-router";
+import sections from '../src/components/component-sections.vue';
+import courses from "../src/components/component-courses.vue";
+import TAs from "../src/components/component-tas.vue";
 import "isomorphic-fetch";
+let expect = require("expect.js");
 
-// describe('component-tas.vue', function () {
-//     it('lifecycle-testing', function () {
-//         // Create a new tas instance
-//         const wrapper = shallow(tas);
-//         // Testing when the fetch is resolved
-//         wrapper.vm.$nextTick(() => {
-//             expect(wrapper.vm.loading).to.be.equal(false);
-//             done();
-//         });
-//     });
-// });
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+const $route = [
+    { path: "/course/:course/ta/:ta", name: "section", component: sections },
+    { path: "/course/:course", name: "TAs", component: TAs },
+    { path: "/", component: courses },
+    { path: "/*", component: courses }
+];
+const router = new VueRouter({
+    routes: $route
+});
+
+describe('component-tas.vue', function () {
+    it('lifecycle-testing', function () {
+        // Create a new sections instance
+        const wrapper = shallow(TAs, {
+            localVue,
+            router
+        });
+        // Testing when the fetch is resolved
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.vm.loading).to.be.equal(false);
+            done();
+        });
+    });
+});
