318f203634f6b739b0e64844fc13329fc39f6af3
Daniel Chen <chen.daniel2099@gmail.com>
2018-05-24 15:46:09 -0400

    removed irrelavant tests

--- test/component-courses.spec.js
+++ test/component-courses.spec.js
@@ -22,20 +22,4 @@ describe("component-courses.vue", function() {
         expect(fetchedJSON.STATUS).to.be.equal("OK");
         expect(Array.isArray(fetchedJSON.DATA)).to.be.true;
     });
-    it("Loaded status should be false given incorrect url", async function() {
-        await wrapper.vm.init("http://localhost:3000/get_info");
-        expect(wrapper.vm.loaded).to.be.equal(false);
-    });
-    it("Loaded status should be true give correct url", async function() {
-        await wrapper.vm.init("http://localhost:3000/get_info.php");
-        expect(wrapper.vm.loaded).to.be.equal(true);
-    });
-    it("Error Status should be false given correct url", async function() {
-        await wrapper.vm.init("http://localhost:3000/get_info.php");
-        expect(wrapper.vm.err).to.be.equal(false);
-    });
-    it("Error Status should be true given incorrect url", async function() {
-        await wrapper.vm.init("http://localhost:3000/get_info.");
-        expect(wrapper.vm.err).to.be.equal(true);
-    });
 });
