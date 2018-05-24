0020b64d9027d8cb131c0220ac307f55a0e2b150
Darren <18633059886@163.com>
2018-05-24 15:24:05 -0400

    Add "return false" statement into the catch block

--- src/components/component-courses.vue
+++ src/components/component-courses.vue
@@ -51,6 +51,7 @@ export default {
             .catch(error => {
                 this.loading = false;
                 this.error = true;
+                return false;
             });
     }
 };
