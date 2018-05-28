0ba36f3992bf628345d3b6475421ad8ed8654e2c
Daniel Chen <chen.daniel2099@gmail.com>
2018-05-28 13:58:56 -0400

    making sure every function returns a query statment to be executed

--- test.php
+++ test.php
@@ -1,27 +1,30 @@
 <?php
-include '../db/config.php';
+include 'db/config.php';
 // get the HTTP method, path and body of the request
 if ISSET($_SERVER['REQUEST_METHOD']){
+	$query = "";
 	$method = $_SERVER['REQUEST_METHOD'];
 	if ($method) {
 		case "POST":
 			$data = $_POST['DATA']
-			handle_post($data)
+			$query = handle_post($data)
 			break;
 	  	case "GET":
 			$data = $_GET['DATA']
-			handle_get($data)
+			$query = handle_get($data)
 			break;
 		case "PUT":
-			$data = 
-			handle_put($data)
+			$data = file_get_contents('php://input')
+			$query = handle_put($data)
 			break;
 		case "DELETE"
-			$data =
-			handle_delete($data)
+			$data = file_get_contents('php://input')
+			$query = handle_delete($data)
 			break;
 	}
 }
+
+
 
 function handle_post (){
 
@@ -40,14 +43,14 @@ $conn = new PDO("mysql:host=$servername;dbname=$databasename", $username, $passw
 
 // set the PDO error mode to exception
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$stmt = $conn->prepare("SELECT DISTINCT Course FROM courses;");
+$stmt = $conn->prepare($query);
 $stmt->execute();
 
 // set the resulting array to associative
-$query = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+$fetched = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
 if (count($query) >= 1) {
 	$result['STATUS'] = 'OK';
-	$result['DATA'] = $query;
+	$result['DATA'] = $fetched;
 } else {
 	$result = array('STATUS' => 'EMPTY');
 }
